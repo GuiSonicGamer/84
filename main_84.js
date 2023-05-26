872de0fe626823b14f14857f94943eb2a84d17e4
GuiSonicGamer <126628777+GuiSonicGamer@users.noreply.github.com>
2023-05-25 21:07:02 -0300

    Add files via upload

--- main_84.js
+++ main_84.js
@@ -3,10 +3,10 @@ pizzaListArray = [];
 function getmenu()
 	{    
         // Dê um nome apropriado ao id como pizzaName
-        var item = document.getElementById("        ").value; 
+        var item = document.getElementById("pizzaName").value; 
        
         //Adicione a função push() para empurrar itens dentro para da array
-        pizzaListArray.      (item);
+        pizzaListArray.push()(item);
         
         var removeCommas = pizzaListArray.join("<br># ");
         document.getElementById("displayMenu").innerHTML =  "# " + removeCommas;
